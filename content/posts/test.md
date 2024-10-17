fff0da686abbe8094bb90c876801388f68462571
Don McLamb <mclamb.donald@gmail.com>
2024-10-16 19:27:56 -0500

    Moved files, changed one to md

--- content/posts/test.md
+++ content/posts/test.md
@@ -1,6 +1,6 @@
 ---
 title: test
 description: testing post from cms
-date: 2024-10-16T19:00:00.000Z
+date: May 4 2023
 ---
 test
--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -6,13 +6,13 @@ import rehypePrism from '@mapbox/rehype-prism';
 import remarkGfm from 'remark-gfm';
 
 // POSTS_PATH is useful when you want to get the path to a specific file
-export const POSTS_PATH = path.join(process.cwd(), 'posts');
+export const POSTS_PATH = path.join(process.cwd(), 'content/posts');
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
 export const postFilePaths = fs
   .readdirSync(POSTS_PATH)
   // Only include md(x) files
-  .filter((path) => /\.mdx?$/.test(path));
+  .filter((path) => /\.(md|mdx)$/.test(path));
 
 export const sortPostsByDate = (posts) => {
   return posts.sort((a, b) => {
